242a1805d1d6be1b993bc272cae68a46d5a8ff5f
Delowar <delowardev@gmail.com>
2020-02-18 06:57:52 +0600

    Webpack Config Updated

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve('lib'),
         filename: 'Progress.js',
-        libraryTarget: 'commonjs2'
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
